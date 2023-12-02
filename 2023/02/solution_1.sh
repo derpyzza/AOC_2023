634ca7864dc683ce42846e86eef661898746e52e
derpyzza <derpyzza@gmail.com>
2023-12-02 14:49:55 +0500

    changes

---
+++ 2023/02/solution_1.sh
@@ -0,0 +1,10 @@
+
+# Read line
+# Check if line matches given constraints
+# Add lines that match given constraints to a list or something
+# Add up the game number of all lines that match the constraints
+# Echo sum;
+
+awk '{print $3, $4}' testCase.txt
+
+echo "$testCase"
--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# AOC 2023
+# AOC
 
-My solutions for Advent of code, 2023.
+My solutions for Advent of code.
